e88826ff8494e40042431b6554329b777656d619
Hypfer <hypfer@nerdstube.de>
2013-08-02 23:31:01 +0200

    Vnstati added to Webinterface

--- web.sh
+++ web.sh
@@ -21,5 +21,21 @@ echo $encoded
 echo "</tr>"
 done < ./conclients
 echo "</table>"
+
+s=`vnstati -i eth0 -s -o /dev/stdout | base64`
+h=`vnstati -i eth0 -h -o /dev/stdout | base64`
+d=`vnstati -i eth0 -d -o /dev/stdout | base64`
+t=`vnstati -i eth0 -t -o /dev/stdout | base64`
+m=`vnstati -i eth0 -m -o /dev/stdout | base64`
+echo "<br>"
+echo "<img src='data:image/png;base64,$s'>"
+echo "<br>"
+echo "<img src='data:image/png;base64,$h'>"
+echo "<br>"
+echo "<img src='data:image/png;base64,$d'>"
+echo "<br>"
+echo "<img src='data:image/png;base64,$t'>"
+echo "<br>"
+echo "<img src='data:image/png;base64,$m'>"
 echo "</body>"
 echo "</html>"
