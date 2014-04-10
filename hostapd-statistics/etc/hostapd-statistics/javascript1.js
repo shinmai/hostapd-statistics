7470976110568014df9174358f7d2d6145ae1b28
Hypfer <Hypfer@users.noreply.github.com>
2014-04-10 16:57:52 +0200

    fixing stuff

--- hostapd-statistics/etc/hostapd-statistics/javascript1.js
+++ hostapd-statistics/etc/hostapd-statistics/javascript1.js
@@ -20,42 +20,25 @@ xmlhttp.open("GET","refreshtable",true);
 xmlhttp.send();
 setTimeout(refreshMainTable, 5000);
 }
-//function refreshVnstati()
-//{
-//if (window.XMLHttpRequest)
-//  {// code for IE7+, Firefox, Chrome, Opera, Safari
-//  xmlhttp=new XMLHttpRequest();
-//  }
-//else
-//  {// code for IE6, IE5
-//  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-//  }
-//xmlhttp.onreadystatechange=function()
-//  {
-//  if (xmlhttp.readyState==4 && xmlhttp.status==200)
-//    {
-//    document.getElementById("VnstatiHolder").innerHTML=xmlhttp.responseText;
-//   }
-//  }
-//xmlhttp.open("GET","refreshvnstati",true);
-//xmlhttp.send();
-//setTimeout(refreshVnstati, 120000);
-//}
-//mediacontrol
-function ajaxMedia(str)
+function refreshVnstati()
 {
 if (window.XMLHttpRequest)
   {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
+  xmlhttp2=new XMLHttpRequest();
   }
 else
   {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  xmlhttp2=new ActiveXObject("Microsoft.XMLHTTP");
   }
-xmlhttp.open("GET",str,true);
+xmlhttp2.onreadystatechange=function()
+  {
+  if (xmlhttp2.readyState==4 && xmlhttp2.status==200)
+    {
+    document.getElementById("VnstatiHolder").innerHTML=xmlhttp2.responseText;
+   }
+  }
+xmlhttp.open("GET","refreshvnstati",true);
 xmlhttp.send();
-
+setTimeout(refreshVnstati, 120000);
 }
-
-
 </script>
