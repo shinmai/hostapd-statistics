af09353824cd5677ca3a3b49bedff07cb2e31139
Aapo Saaristo <shinmai@subexit.com>
2015-11-10 18:25:55 +0200

    remove .deb file and fix build script

--- build_deb.sh
+++ build_deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo chmod +x ./hostapd-statistics/etc/init.d/hostapd-statistics
-sudo chmod +x ./hostapd-statistics/usr/bin/hostapd-statistics
+sudo chmod +x ./hostapd-statistics/usr/sbin/hostapd-statistics
 sudo chown root:root ./hostapd-statistics/
 dpkg-deb --build ./hostapd-statistics
