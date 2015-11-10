0b969dba04c903fbb605b4526db305fd8e56b8b5
Aapo Saaristo <shinmai@subexit.com>
2015-11-10 18:10:06 +0200

    Change font to be easier on the eyes
    Google Web Font "Varela Round"

--- hostapd-statistics/etc/hostapd-statistics/javascript1.js
+++ hostapd-statistics/etc/hostapd-statistics/javascript1.js
@@ -43,3 +43,16 @@ setTimeout(refreshVnstati, 120000);
 }
 refreshVnstati();
 </script>
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Varela+Round::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
